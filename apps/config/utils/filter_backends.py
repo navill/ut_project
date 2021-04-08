9207dfc6890c813544a140608b0ce076c092834c
jihoon <blue_jihoon@naver.com>
2021-04-08 17:36:41 +0900

    - 나이 검색에 사용되는 create_extra_expression() 함수 코드 라인 수정

--- apps/config/utils/filter_backends.py
+++ apps/config/utils/filter_backends.py
@@ -16,8 +16,8 @@ def create_extra_expression(target_expressions: Dict[str, int]) -> str:
     for name, value in target_expressions.items():
         expr_container.append(f"{function_name} {LOOKUP_OPERATOR[name]} {value}")
 
-    extra_expression = f"{expr_container[0]} AND {expr_container[1]}" if len(expr_container) == 2 \
-        else expr_container[0]
+    extra_expression = f"{expr_container[0]} AND {expr_container[1]}" \
+        if len(expr_container) == 2 else expr_container[0]
 
     return extra_expression
 

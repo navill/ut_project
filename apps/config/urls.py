a4edbbe71ab855d41d468c79afb9fbb175247b95
jihoon <blue_jihoon@naver.com>
2021-05-20 20:20:11 +0900

    임시 파일 업로드&다운로드 url 추가

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 
 from accounts.api.views import AccountsTokenPairView, TokenLogoutView, AccountsTokenRefreshView, session_logout_view
 from config.utils.doc_utils import schema_view
+from files.api.views import TempFiles, TempFilesUpload, TempFilesDownload, TempFilesBulkDownload
 
 urlpatterns = [
     path('core-api/', include('core.api.urls', namespace='core-api')),
@@ -29,6 +30,13 @@ urlpatterns += [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+urlpatterns += [
+    path('chosun/files', TempFiles.as_view(), name='temp-files'),
+    path('chosun/files/upload', TempFilesUpload.as_view(), name='temp-files-upload'),
+    path('chosun/files/<int:pk>/download', TempFilesDownload.as_view(), name='temp-files-download'),
+    path('chosun/files/download', TempFilesBulkDownload.as_view(), name='temp-files-upload')
 ]
 
 if settings.DEBUG:
